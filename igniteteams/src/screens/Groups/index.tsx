a7dc3c9a1056ac72b532f3562b71aa78f79bd49e
Thiago Pereira <pereira.tp.thiago@gmail.com>
2023-02-24 17:40:44 -0300

    Criando componente button

--- igniteteams/src/screens/Groups/index.tsx
+++ igniteteams/src/screens/Groups/index.tsx
@@ -6,6 +6,7 @@ import { Highlight } from "@components/Highlight";
 import { Container } from "./styles";
 import { FlatList } from "react-native";
 import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([]);
@@ -25,6 +26,7 @@ export function Groups() {
           <ListEmpty message="Que tal cadastrar a primeira turma?" />
         )}
       />
+      <Button title="Criar nova turma"  />
     </Container>
   );
 }
